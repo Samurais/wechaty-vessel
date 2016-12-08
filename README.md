1dd74582a113f8d7180729b0f9af20f9c7b08c5e
Samurais <hailiang.hl.wang@gmail.com>
2016-12-08 19:28:26 +0800

    update .gitmodules and install wechaty with npm

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 ```
 git clone git@github.com:Samurais/wechaty-vessel.git
 git submodule update --remote --checkout --init --recursive
-
+cd wechaty
+npm install && npm run dist
 ```
+
--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,92 @@ const log4js = require('log4js');
 const logger = log4js.getLogger();
 
 logger.info('log4js', config.env);
+
+/**
+ *
+ * Wechaty - Wechat for Bot
+ *
+ * Connecting ChatBots
+ * https://github.com/wechaty/wechaty
+ *
+ */
+
+/* tslint:disable:variable-name */
+const QrcodeTerminal = require('qrcode-terminal')
+const nodeCleanup = require('node-cleanup')
+
+import {
+    Wechaty
+    , Config
+    , log
+} from 'wechaty';
+
+const welcome = `
+| __        __        _           _
+| \\ \\      / /__  ___| |__   __ _| |_ _   _
+|  \\ \\ /\\ / / _ \\/ __| '_ \\ / _\` | __| | | |
+|   \\ V  V /  __/ (__| | | | (_| | |_| |_| |
+|    \\_/\\_/ \\___|\\___|_| |_|\\__,_|\\__|\\__, |
+|                                     |___/
+
+=============== Powered by Wechaty ===============
+-------- https://github.com/zixia/wechaty --------
+
+I'm a bot, my super power is talk in Wechat.
+
+If you send me a 'ding', I will reply you a 'dong'!
+__________________________________________________
+
+Hope you like it, and you are very welcome to
+upgrade me for more super powers!
+
+Please wait... I'm trying to login in...
+
+`
+
+console.log(welcome)
+const bot = Wechaty.instance({ profile: Config.DEFAULT_PROFILE })
+
+bot
+    .on('login', user => log.info('Bot', `${user.name()} logined`))
+    .on('logout', user => log.info('Bot', `${user.name()} logouted`))
+    .on('error', e => log.info('Bot', 'error: %s', e))
+    .on('scan', (url, code) => {
+        if (!/201|200/.test(String(code))) {
+            let loginUrl = url.replace(/\/qrcode\//, '/l/')
+            QrcodeTerminal.generate(loginUrl)
+        }
+        console.log(`${url}\n[${code}] Scan QR Code in above url to login: `)
+    })
+    .on('message', m => {
+        try {
+            const room = m.room()
+            console.log((room ? '[' + room.topic() + ']' : '')
+                + '<' + m.from().name() + '>'
+                + ':' + m.toStringDigest()
+            )
+
+            if (/^(ding|ping|bing)$/i.test(m.content()) && !m.self()) {
+                m.say('dong')
+                log.info('Bot', 'REPLY: dong')
+            }
+        } catch (e) {
+            log.error('Bot', 'on(message) exception: %s', e)
+        }
+    })
+
+bot.init()
+    .then(() => {
+        bot.say('Wechaty init')
+    })
+    .catch(e => {
+        log.error('Bot', 'init() fail: %s', e)
+        bot.quit()
+        process.exit(-1)
+    })
+
+nodeCleanup((reason, code) => {
+    const exitMsg = `Wechaty exit ${code} because of ${reason} `
+    console.log(exitMsg)
+    bot.say(exitMsg)
+})
