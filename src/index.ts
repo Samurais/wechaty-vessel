002d71122eafd1f98687749382f745b15d3d99bd
Samurais <hailiang.hl.wang@gmail.com>
2016-12-08 18:23:30 +0800

    add config module

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 /**
  * Index 
  */
+import config from './config/environment';
 const log4js = require('log4js');
 const logger = log4js.getLogger();
 
-logger.info('foo');
+logger.info('log4js', config.env);
